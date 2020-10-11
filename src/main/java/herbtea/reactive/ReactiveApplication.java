2da8dfd8e9fe2e6188f2d0a16f4686e7bdf0b09a
herbtea <herbtea30@gmail.com>
2020-10-11 16:53:35 +0900

    Java Reactive Streams basic (4) - toby Java Async feature and Spring Async feature

--- src/main/java/herbtea/reactive/ReactiveApplication.java
+++ src/main/java/herbtea/reactive/ReactiveApplication.java
@@ -1,14 +1,29 @@
 package herbtea.reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Future;
+
 @SpringBootApplication
+@Slf4j
+@EnableAsync
 public class ReactiveApplication {
     @RestController
     public static class Controller {
@@ -34,8 +49,43 @@ public class ReactiveApplication {
         }
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReactiveApplication.class, args);
+    @Component
+    public static class MyService {
+        @Async(value = "tp")
+        public ListenableFuture<String> hello() throws InterruptedException {
+            log.info("hello");
+            Thread.sleep(2000);
+            return new AsyncResult<>("Hello");
+        }
     }
 
+    @Bean
+    ThreadPoolTaskExecutor tp() {
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(10); //첫 요청시에 10개를 만듬
+        te.setMaxPoolSize(100); //큐가 꽉 차면 그때 풀사이즈를 늘려줌
+        te.setQueueCapacity(200);
+        te.setThreadNamePrefix("myThread");
+        te.initialize();
+        return te;
+    }
+
+    public static void main(String[] args) {
+        try(ConfigurableApplicationContext c = SpringApplication.run(ReactiveApplication.class, args)){
+        }
+    }
+
+    @Autowired MyService myService;
+
+    @Bean
+    ApplicationRunner run() {
+        return args -> {
+            log.info("run()");
+            ListenableFuture<String> f = myService.hello();
+            f.addCallback(s-> System.out.println(s), e-> System.out.println(e.getMessage()));
+            log.info("exit");
+//            log.info("exit : " + f.isDone());
+//            log.info("result: " + f.get());
+        };
+    }
 }
