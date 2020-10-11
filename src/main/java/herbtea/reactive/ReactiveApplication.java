af347334fa6811ab14772849bc4351094dca2da0
herbtea <herbtea30@gmail.com>
2020-10-11 18:13:12 +0900

    Java Reactive Streams basic (4) - toby DeferredResult

--- src/main/java/herbtea/reactive/ReactiveApplication.java
+++ src/main/java/herbtea/reactive/ReactiveApplication.java
@@ -1,91 +1,58 @@
 package herbtea.reactive;
 
 import lombok.extern.slf4j.Slf4j;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
-import java.util.concurrent.Future;
+import java.util.Queue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @SpringBootApplication
 @Slf4j
 @EnableAsync
 public class ReactiveApplication {
     @RestController
-    public static class Controller {
-        @RequestMapping("hello")
-        public Publisher<String> hello(String name) {
-            return new Publisher<String>() {
-                @Override
-                public void subscribe(Subscriber<? super String> subscriber) {
-                    subscriber.onSubscribe(new Subscription() {
-                        @Override
-                        public void request(long l) {
-                            subscriber.onNext("Hello " + name);
-                            subscriber.onComplete();
-                        }
+    public static class  MyConstroller {
+        Queue<DeferredResult<String>> results = new ConcurrentLinkedQueue<>();
+//        @GetMapping("/callable")
+//        public Callable<String> callable() throws InterruptedException {
+//            log.info("callable");
+//            return () -> {
+//                log.info("async");
+//                Thread.sleep(2000);
+//                return "hello";
+//            };
+//        }
 
-                        @Override
-                        public void cancel() {
-
-                        }
-                    });
-                }
-            };
+        @GetMapping("/dr")
+        public DeferredResult<String> deferredResult() throws InterruptedException {
+            log.info("dr");
+            DeferredResult<String> dr = new DeferredResult<>(600000L);
+            results.add(dr);
+            return dr;
         }
-    }
 
-    @Component
-    public static class MyService {
-        @Async(value = "tp")
-        public ListenableFuture<String> hello() throws InterruptedException {
-            log.info("hello");
-            Thread.sleep(2000);
-            return new AsyncResult<>("Hello");
+        @GetMapping("dr/count")
+        public String drCount() {
+            return String.valueOf(results.size());
         }
-    }
 
-    @Bean
-    ThreadPoolTaskExecutor tp() {
-        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-        te.setCorePoolSize(10); //첫 요청시에 10개를 만듬
-        te.setMaxPoolSize(100); //큐가 꽉 차면 그때 풀사이즈를 늘려줌
-        te.setQueueCapacity(200);
-        te.setThreadNamePrefix("myThread");
-        te.initialize();
-        return te;
+        @GetMapping("dr/event")
+        public String drEvent(String msg) {
+            for(DeferredResult<String> dr : results) {
+                dr.setResult("Hello " + msg);
+                results.remove(dr);
+            }
+            return "OK";
+        }
     }
 
     public static void main(String[] args) {
-        try(ConfigurableApplicationContext c = SpringApplication.run(ReactiveApplication.class, args)){
-        }
-    }
-
-    @Autowired MyService myService;
-
-    @Bean
-    ApplicationRunner run() {
-        return args -> {
-            log.info("run()");
-            ListenableFuture<String> f = myService.hello();
-            f.addCallback(s-> System.out.println(s), e-> System.out.println(e.getMessage()));
-            log.info("exit");
-//            log.info("exit : " + f.isDone());
-//            log.info("result: " + f.get());
-        };
+        SpringApplication.run(ReactiveApplication.class, args);
     }
 }
